2b557cee3c53d6dcbe7909ab340a15cc93a716d3
Chan <95647484+Chan-log@users.noreply.github.com>
2022-07-19 23:04:49 +0900

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -1,7 +1,7 @@
 let arr = ['a', 'b', 'c', 'd', 'e'];
 let arr2 = [1,2,3,4];
 
-// Array
+// Array의 모든 값이 정수라면 평균 값을 구해준다.
 Array.prototype.average = function (){
     if(this.every((x) => typeof(x) === "number")){
         return this.reduce((p, c) => p + c) / this.length;
