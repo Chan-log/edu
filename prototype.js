cd5f30af75ed608ab2422b8aedf5201d2888bf18
Chan <95647484+Chan-log@users.noreply.github.com>
2022-07-19 23:04:27 +0900

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -1,6 +1,7 @@
 let arr = ['a', 'b', 'c', 'd', 'e'];
 let arr2 = [1,2,3,4];
 
+// Array
 Array.prototype.average = function (){
     if(this.every((x) => typeof(x) === "number")){
         return this.reduce((p, c) => p + c) / this.length;
@@ -9,3 +10,5 @@ Array.prototype.average = function (){
     };
 };
 
+// arr.average(); return -1
+// arr2.average(); return 2.5
